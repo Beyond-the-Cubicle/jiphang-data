b16b266a8a49811a843fbe7e613db9647f4f3cbf
owljoa <needshield@gmail.com>
2024-01-08 22:36:51 +0900

    서울 버스 정류장 데이터, TAGO 버스 정류장 데이터 수집 로직 추가
    - 서울, 경기 버스정류장 정보 별도 API로 수집 시 stationId가 겹치는 경우 발생 -> 여기서 stationId가 겹치는데 ARS ID가 다른 경우가 발생
    - 그래서 전국 데이터인 TAGO 데이터도 추가 후 서울, 경기 별도 수집은 주석처리

--- collector/busstation/dto/busstation.go
+++ collector/busstation/dto/busstation.go
@@ -2,11 +2,11 @@ package dto
 
 import "strconv"
 
-type RawOpenAPIResponse struct {
+type GyunggiRawOpenAPIResponse struct {
 	BusStation []interface{}
 }
 
-type OpenAPIBusStation struct {
+type GyunggiOpenAPIBusStation struct {
 	SIGUN_NM            string // 시군명
 	SIGUN_CD            string // 시군코드
 	STATION_NM_INFO     string // 정류소명
@@ -20,7 +20,7 @@ type OpenAPIBusStation struct {
 	WGS84_LOGT          string // WGS84 경도
 }
 
-func (openApiBusStation *OpenAPIBusStation) ToBusStation() BusStation {
+func (openApiBusStation *GyunggiOpenAPIBusStation) ToBusStation() BusStation {
 	latitude, _ := strconv.ParseFloat(openApiBusStation.WGS84_LAT, 64)
 	longitude, _ := strconv.ParseFloat(openApiBusStation.WGS84_LOGT, 64)
 
@@ -71,4 +71,76 @@ type BusStation struct {
 	Longitude           float64
 	CityCode            string
 	CityName            string
+}
+
+type SeoulRawOpenAPIResponse struct {
+	TbisMasterStation TbisMasterStation
+}
+
+type TbisMasterStation struct {
+	List_total_count int
+	Result           OpenAPIResultCode
+	Row              []SeoulOpenAPIBusStation
+}
+
+type SeoulOpenAPIBusStation struct {
+	STTN_ID               string  // 정류장 ID
+	STTN_NM               string  // 정류장 명칭
+	STTN_TYPE             string  // 정류장 유형 (중앙차로 여부 (일반차로, 중앙차로 ...))
+	STTN_NO               string  // 정류장 번호(ARS ID)
+	CRDNT_X               float64 // 경도
+	CRDNT_Y               float64 // 위도
+	BUSINFO_FCLT_INSTL_YN string  // 버스도착정보아낸기 설치 여부
+}
+
+func (openApiBusStation *SeoulOpenAPIBusStation) ToBusStation() BusStation {
+	return BusStation{
+		StationName: openApiBusStation.STTN_NM,
+		StationId:   openApiBusStation.STTN_ID,
+		ArsId:       openApiBusStation.STTN_NO,
+		Latitude:    openApiBusStation.CRDNT_Y,
+		Longitude:   openApiBusStation.CRDNT_X,
+	}
+}
+
+type TagoOpenAPIResponse struct {
+	CurrentCount int
+	Page         int
+	PerPage      int
+	TotalCount   int
+	MatchCount   int
+	Data         []TagoOpenAPIBusStation
+}
+
+type TagoOpenAPIBusStation struct {
+	StationId         string `json:"정류장번호"`
+	StationName       string `json:"정류장명"`
+	Latitude          string `json:"위도"`
+	Longitude         string `json:"경도"`
+	CollectedDate     string `json:"정보수집일"`
+	MobileShortNumber int    `json:"모바일단축번호"`
+	CityCode          int    `json:"도시코드"`
+	CityName          string `json:"도시명"`
+	ManagerCityName   string `json:"관리도시명"`
+}
+
+func (openApiBusStation *TagoOpenAPIBusStation) ToBusStation() BusStation {
+	latitude, _ := strconv.ParseFloat(openApiBusStation.Latitude, 64)
+	longitude, _ := strconv.ParseFloat(openApiBusStation.Longitude, 64)
+
+	return BusStation{
+		CityName:            openApiBusStation.CityName,
+		CityCode:            strconv.Itoa(openApiBusStation.CityCode),
+		StationName:         openApiBusStation.StationName,
+		StationId:           openApiBusStation.StationId,
+		ArsId:               strconv.Itoa(openApiBusStation.MobileShortNumber),
+		GovermentOfficeName: openApiBusStation.ManagerCityName,
+		Latitude:            latitude,
+		Longitude:           longitude,
+	}
+}
+
+type TagoFailResponse struct {
+	Code int
+	Msg  string
 }
--- collector/busstation/main.go
+++ collector/busstation/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/Beyond-the-Cubicle/cgp-data/collector/busstation/config"
 	"github.com/Beyond-the-Cubicle/cgp-data/collector/busstation/database"
@@ -14,21 +15,106 @@ import (
 
 func main() {
 	database.ConnectDb()
-	collectGynggiBusStations()
-
+	deleteAllBusStations()
+	// TODO: TAGO가 아닌 서울, 경기만 별도로 하려면, 겹치는 stationId 해결해야함 -> 문제는 stationId 겹칠 때 ARS ID가 다른 경우가 있음
+	// collectSeoulBusStations()
+	// collectGynggiBusStations()
+	collectTagoBusStations()
 	database.Db.Close()
+
+}
+
+func collectSeoulBusStations() {
+	fmt.Printf("=============== 서울 버스 정류장 수집 시작 ===============\n")
+	var apiError dto.OpenAPIError
+	startIndex := 1
+	endIndex := 1000
+	var busStations []dto.BusStation
+
+	// 정류장 전체 카운트 구하기
+	responseForCount, apiError, _ := requestSeoulBusStations(1, 1)
+	if (responseForCount.TbisMasterStation.List_total_count == 0 && apiError != dto.OpenAPIError{}) {
+		fmt.Printf("[에러 응답 수신] URL: %s, code: %s, message: %s\n", apiError.Url, apiError.Result.Code, apiError.Result.Message)
+		return
+	}
+	totalCount := responseForCount.TbisMasterStation.List_total_count
+	fmt.Printf("서울 수집 대상 버스 정류장 갯수: %d\n", totalCount)
+
+	for {
+		response, apiError, url := requestSeoulBusStations(startIndex, endIndex)
+		if (response.TbisMasterStation.List_total_count == 0 && apiError != dto.OpenAPIError{}) {
+			fmt.Printf("[에러 응답 수신] URL: %s, code: %s, message: %s\n", apiError.Url, apiError.Result.Code, apiError.Result.Message)
+			return
+		}
+
+		if response.TbisMasterStation.Result.Code != "INFO-000" {
+			fmt.Printf("[정상 처리되지 않은 응답코드 수신] URL: %s, ResultCode: %#v\n", url, response.TbisMasterStation.Result)
+			continue
+		}
+
+		for _, seoulOpenAPIBusStation := range response.TbisMasterStation.Row {
+			busStations = append(busStations, seoulOpenAPIBusStation.ToBusStation())
+		}
+
+		// 전체 카운트보다 현재까지 수행한 endIndex가 크면 완료 처리
+		if endIndex > totalCount {
+			break
+		}
+
+		startIndex += 1000
+		endIndex += 1000
+	}
+
+	fmt.Printf("[서울 버스 정상 수집 완료] Collected BusStation Count: %d\n", len(busStations))
+
+	// DB에 저장
+	insertBusStations(busStations)
+	fmt.Printf("서울 버스 정류장 정보 DB 저장 완료\n")
+	fmt.Printf("=============== 서울 버스 정류장 수집 종료 ===============\n")
+}
+
+func requestSeoulBusStations(startIndex int, endIndex int) (dto.SeoulRawOpenAPIResponse, dto.OpenAPIError, string) {
+	var apiError dto.OpenAPIError
+	var openAPIFailResponse dto.OpenAPIFailResponse
+	var rawOpenApiResponse dto.SeoulRawOpenAPIResponse
+
+	url := fmt.Sprintf("http://openapi.seoul.go.kr:8088/%s/%s/tbisMasterStation/%d/%d", config.SeoulApiKey, config.DocType, startIndex, endIndex)
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	jsonByte, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// 정상 응답이 아닌 케이스
+	json.Unmarshal(jsonByte, &openAPIFailResponse)
+	if openAPIFailResponse.Result.Code != "" {
+		apiError = dto.OpenAPIError{
+			Url:    url,
+			Result: openAPIFailResponse.Result,
+		}
+		return dto.SeoulRawOpenAPIResponse{}, apiError, url
+	}
+
+	json.Unmarshal(jsonByte, &rawOpenApiResponse)
+	return rawOpenApiResponse, apiError, url
 }
 
 func collectGynggiBusStations() {
-
+	fmt.Printf("=============== 경기도 버스 정류장 수집 시작 ===============\n")
 	var apiError dto.OpenAPIError
 	pageSize := 1000
 	pageIndex := 1
 	var openAPIFailResponse dto.OpenAPIFailResponse
-	var rawOpenApiResponse dto.RawOpenAPIResponse
+	var rawOpenApiResponse dto.GyunggiRawOpenAPIResponse
 	var busStations []dto.BusStation
 	for {
-		url := fmt.Sprintf("https://openapi.gg.go.kr/BusStation?Key=%s&Type=%s&pIndex=%d&pSize=%d", config.ApiKey, config.DocType, pageIndex, pageSize)
+		url := fmt.Sprintf("https://openapi.gg.go.kr/BusStation?Key=%s&Type=%s&pIndex=%d&pSize=%d", config.GyunggiApiKey, config.DocType, pageIndex, pageSize)
 		response, err := http.Get(url)
 		if err != nil {
 			panic(err)
@@ -70,14 +156,15 @@ func collectGynggiBusStations() {
 		return
 	}
 
-	fmt.Printf("[정상 수집 완료] Collected BusStation Count: %d\n", len(busStations))
+	fmt.Printf("[경기도 버스 정상 수집 완료] Collected BusStation Count: %d\n", len(busStations))
 
 	// DB에 저장
 	insertBusStations(busStations)
-	fmt.Printf("경기도 버스 정류장 정보 DB 저장 완료")
+	fmt.Printf("경기도 버스 정류장 정보 DB 저장 완료\n")
+	fmt.Printf("=============== 경기도 버스 정류장 수집 종료 ===============\n")
 }
 
-func makeOpenApiResponseHead(rawOpenApiResponse dto.RawOpenAPIResponse) dto.OpenAPIResponseHead {
+func makeOpenApiResponseHead(rawOpenApiResponse dto.GyunggiRawOpenAPIResponse) dto.OpenAPIResponseHead {
 	rawOpenApiHeadMapList := rawOpenApiResponse.BusStation[0].(map[string]interface{})["head"].([]interface{})
 
 	// list_total_count
@@ -102,11 +189,11 @@ func makeOpenApiResponseHead(rawOpenApiResponse dto.RawOpenAPIResponse) dto.Open
 	return head
 }
 
-func makeBusStations(rawOpenApiResponse dto.RawOpenAPIResponse) []dto.BusStation {
+func makeBusStations(rawOpenApiResponse dto.GyunggiRawOpenAPIResponse) []dto.BusStation {
 	// openApiResponse의 두번째 요소가 row를 포함하는 map 구조로 실제 정류소 정보 리스트를 포함
 	rawOpenApiRowMapList := rawOpenApiResponse.BusStation[1].(map[string]interface{})["row"]
 
-	var openApiBusStations []dto.OpenAPIBusStation
+	var openApiBusStations []dto.GyunggiOpenAPIBusStation
 	err := mapstructure.Decode(rawOpenApiRowMapList, &openApiBusStations)
 	if err != nil {
 		panic(err)
@@ -133,4 +220,93 @@ func insertBusStations(busStations []dto.BusStation) {
 			panic(err)
 		}
 	}
+}
+
+func deleteAllBusStations() {
+	fmt.Printf("=============== 버스 정류장 전체 데이터 제거 시작 ===============\n")
+	_, err := database.Db.Exec("DELETE FROM bus_station")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("=============== 버스 정류장 전체 데이터 제거 종료 ===============\n")
+}
+
+func collectTagoBusStations() {
+	fmt.Printf("=============== TAGO 버스 정류장 수집 시작 ===============\n")
+	var apiError dto.OpenAPIError
+	pageIndex := 1
+	pageSize := 5000
+	var busStations []dto.BusStation
+
+	// 정류장 전체 카운트 구하기
+	responseForCount, apiError := requestTagoBusStations(1, 1)
+	if (responseForCount.TotalCount == 0 && apiError != dto.OpenAPIError{}) {
+		fmt.Printf("[에러 응답 수신] URL: %s, code: %s, message: %s\n", apiError.Url, apiError.Result.Code, apiError.Result.Message)
+		return
+	}
+	totalCount := responseForCount.TotalCount
+	fmt.Printf("TAGO 수집 대상 버스 정류장 갯수: %d\n", totalCount)
+
+	for {
+		response, apiError := requestTagoBusStations(pageIndex, pageSize)
+		if (response.TotalCount == 0 && apiError != dto.OpenAPIError{}) {
+			fmt.Printf("[에러 응답 수신] URL: %s, code: %s, message: %s\n", apiError.Url, apiError.Result.Code, apiError.Result.Message)
+			return
+		}
+
+		for _, tagoOpenAPIBusStation := range response.Data {
+			busStations = append(busStations, tagoOpenAPIBusStation.ToBusStation())
+		}
+
+		fmt.Printf("pageIndex: %d\n", pageIndex)
+
+		// 전체 카운트만큼 수집되면 중지
+		if len(busStations) >= totalCount {
+			break
+		}
+
+		pageIndex += 1
+	}
+
+	fmt.Printf("[TAGO 버스 정상 수집 완료] Collected BusStation Count: %d\n", len(busStations))
+
+	// DB에 저장
+	insertBusStations(busStations)
+	fmt.Printf("TAGO 버스 정류장 정보 DB 저장 완료\n")
+	fmt.Printf("=============== TAGO 버스 정류장 수집 종료 ===============\n")
+}
+
+func requestTagoBusStations(pageIndex int, pageSize int) (dto.TagoOpenAPIResponse, dto.OpenAPIError) {
+	var apiError dto.OpenAPIError
+	var tagoFailResponse dto.TagoFailResponse
+	var tagoOpenAPIResponse dto.TagoOpenAPIResponse
+
+	url := fmt.Sprintf("https://api.odcloud.kr/api/15067528/v1/uddi:eb02ec03-6edd-4cb0-88b8-eda22ca55e80?page=%d&perPage=%d&serviceKey=%s", pageIndex, pageSize, config.TagoApiKey)
+	response, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	jsonByte, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// 정상 응답이 아닌 케이스
+	json.Unmarshal(jsonByte, &tagoFailResponse)
+	if tagoFailResponse.Code != 0 {
+		apiError = dto.OpenAPIError{
+			Url: url,
+			Result: dto.OpenAPIResultCode{
+				Code:    strconv.Itoa(tagoFailResponse.Code),
+				Message: tagoFailResponse.Msg,
+			},
+		}
+		return dto.TagoOpenAPIResponse{}, apiError
+	}
+
+	json.Unmarshal(jsonByte, &tagoOpenAPIResponse)
+	return tagoOpenAPIResponse, apiError
 }
